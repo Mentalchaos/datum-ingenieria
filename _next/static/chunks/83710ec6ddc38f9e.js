6de1fc889111dd77f423d762bf5637679fae8199
JP Vera <jean.vera@simplit-solutions.com>
2025-11-12 19:03:24 -0300

    Updates

--- _next/static/chunks/83710ec6ddc38f9e.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/6c7890a861cc4121.js",
-  "static/chunks/bc7421d54b698516.js",
-  "static/chunks/0db576a575597798.js",
-  "static/chunks/9c34cd5dd7e60c0a.js",
-  "static/chunks/7fd85a8c6294ca6f.css",
-  "static/chunks/turbopack-a03cb0f50a93caee.js"
-])
---
+++ _next/static/chunks/c3de9d7536132125.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/0412a8e97b7a9777.js",
+  "static/chunks/bc7421d54b698516.js",
+  "static/chunks/9c34cd5dd7e60c0a.js",
+  "static/chunks/0db576a575597798.js",
+  "static/chunks/6b164da2646c4193.css",
+  "static/chunks/turbopack-ba7ba914c224f0fe.js"
+])
